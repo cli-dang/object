be02c6065e8317e9edc74690db694cd083686817
Simone Del Popolo <simonedelpopolo@outlook.com>
2023-05-07 15:15:24 +0000

    Initial commit

---
+++ shell-script/prePublish.sh
@@ -0,0 +1,8 @@
+rm -rf ./lib || true
+rm -f ./index.js || true
+rm -rf ./tests || true
+rm -rf ./coverage || true
+rm -rf ./node_modules || true
+rm -rf ./package-lock.json || true
+npm install
+npm run build
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export { object_to_json } from './lib/object/object_to_json.js'
+export { object_to_string } from './lib/object/object_to_string.js'
+export { string_to_object } from './lib/object/string_to_object.js'
